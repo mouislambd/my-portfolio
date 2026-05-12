173a9fc225fa4ad59dabbcb677641a1c3f6dfc27
mouislambd <tanhaislammou097@gmail.com>
2026-05-12 06:00:35 -0700

    removing old skills added a new skills

--- app/portfolio.tsx
+++ app/portfolio.tsx
@@ -213,7 +213,7 @@ const ME = {
 
 const SKILLS = [
     { icon: "", title: "FRONTEND", items: ["HTML5 & Semantic CSS", "Tailwind CSS", "JavaScript (ES6+)", "React.js", "Next.js"] },
-    { icon: "", title: "BACKEND & TOOLS  ", items: ["Python (Learning)", "DOM Manipulation","node.js", "express.js", "MongoDB", "Firebase", "REST API Integration", "Git & GitHub", "vercel",] },
+    { icon: "", title: "BACKEND & TOOLS  ", items: ["Python (Learning)", "node.js", "express.js", "MongoDB", "Firebase", "REST API Integration", "Git & GitHub", "vercel",] },
     { icon: "", title: "FUTURE STACK", items: ["Data Science (Upcoming)", "Machine Learning", "NumPy & Pandas", "Data Visualization", " aspiring AI Engineering"] },
 ];
 
